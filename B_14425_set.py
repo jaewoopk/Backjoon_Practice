a3272457bf0430a89126689cc2a102450305f87b
CMP <77138149+cmp42@users.noreply.github.com>
2022-07-08 16:44:54 +0900

    Day 4 Solve none question about Binary Search
    After dinner to solve this question

---
+++ B_14425_set.py
@@ -0,0 +1,32 @@
+import sys
+
+def BinarySearch(arr, check, start, end, point) :
+    while (start <= end) :
+        mid = (start + end) // 2
+        if (arr[mid] == check[point]) :
+            return 1
+        elif (arr[mid] > check[point]) :
+            start = mid
+        else :
+            end = mid - 1
+    return 0
+
+n, m = map(int, sys.stdin.readline().split())
+
+arr = []
+check = []
+
+count = 0
+
+for i in range(n) :
+    arr.append(input())
+
+for i in range(m) :
+    check.append(input())
+
+arr.sort()
+
+for i in range(m) :
+    count += BinarySearch(arr,check,0,n - 1,i)
+
+print(count)
