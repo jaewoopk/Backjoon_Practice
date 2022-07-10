1b0fbb2b52a9ffd2f9f70ef7bf199e6cedb006b4
jaewoopk <jaewoopk000@naver.com>
2022-07-10 15:40:01 +0900

    Day 6 Execpt Binary search and Do only dictionary but failed

--- B_1620_set.py
+++ B_1620_set.py
@@ -8,38 +8,23 @@ def StringToInt(s) :
     except :
         return 0
 
-def BinarySearch(poketbook, poketbook2, check, start, end, point) :
-    while (start <= end) :
-        mid = (start + end) // 2
-        if (poketbook2[mid] == check[point]) :
-            print(poketbook[poketbook2[mid]] + 1)
-            return
-        elif (poketbook2[mid] < check[point]) :
-            start = mid + 1
-        else :
-            end = mid - 1
-    return
-
 n, m = map(int, sys.stdin.readline().split())
 
 book = {}
 check = []
 
 for i in range(n) :
-    poketmon = input()
-    book.setdefault(poketmon,i)
+    book.setdefault(input(),i)
 
 for i in range(m) :
     check.append(input())
 
 realbook = list(book.keys())
-poketbook = dict(sorted(book.items()))
-poketbook2 = list(poketbook.keys())
 
 for i in range(m) :
     checker = StringToInt(check[i])
     if (checker) :
         print(realbook[checker - 1])
     else :
-        BinarySearch(poketbook, poketbook2, check, 0, n - 1, i)
+        print(book.get(check[i]) + 1)
 
