41fa3346ce3ef9fde32196d82c2ef7d145a64e3a
jaewoopk <jaewoopk000@naver.com>
2022-08-01 16:42:02 +0900

    Day 12 Solve 1 question with binary set about list

--- B_1764_set.py
+++ B_1764_set.py
@@ -1,14 +1,39 @@
 import sys
 
+def BinarySearch(a, b, start, end, point) :
+    while (start <= end) :
+        mid = (start + end) // 2
+        if (a[mid] == b[point]) :
+            return 1
+        elif (a[mid] < b[point]) :
+            start = mid + 1
+        else :
+            end = mid - 1
+    return 0
+
+result = 0
 a = []
 b = []
+c = []
 
 n, m = map(int, input().split())
 
 for i in range(n) :
-    a.append(sys.stdin.readline.strip())
+    a.append(sys.stdin.readline().rstrip())
 
 for i in range(m) :
-    b.append(sys.stdin.readline.strip())
+    b.append(sys.stdin.readline().rstrip())
 
-print(a)
+a.sort()
+b.sort()
+
+for i in range(m) :
+    if (BinarySearch(a, b, 0, n - 1, i)) :
+        c.append(b[i])
+        result += 1
+
+print(result)
+
+for i in c :
+    print(i)
+
