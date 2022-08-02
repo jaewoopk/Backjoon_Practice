a00b8e0e192e57e9dba1517e9dc7bc2cc1db66df
Dus_JW <jaewoopk000@naver.com>
2022-08-02 15:25:55 +0900

    Day 13 Solve no question about math but trying to solve it

--- B_2477_math.py
+++ B_2477_math.py
@@ -1,9 +1,35 @@
 k = int(input())
 
 a = []
-for i in range(6) :
+b = []
+c = []
+count = 6
+for i in range(count) :
     a.append(input().split())
+    
+for i in range(count) :
+    for j in range(i + 1, count) :
+        if (a[i][0] == a[j][0] and int(a[i][1]) < int(a[j][1])) :
+            b.append(a[i])
+            break
 
+        elif (a[i][0] == a[j][0] and int(a[i][1]) >= int(a[j][1])) :
+            b.append(a[j])
+            break
+
+
+for i in range(6) :
+    if (a[i][0] != b[0][0] and a[i][0] != b[1][0]) :
+        c.append(a[i])
+        
+print(a)
+print(b)
+print(c)
+tmp = int(c[0][1]) * int(c[1][1]) - int(b[0][1]) * int(b[1][1])
+
+print(tmp * k)
+
+'''
 for i in range(5) :
     if (a[i][0] == '4' and a[i + 1][0] == '1') :
         check = 1
@@ -42,3 +68,4 @@ for i in range(5) :
         answer = 0
 
 print(answer * k)
+'''
