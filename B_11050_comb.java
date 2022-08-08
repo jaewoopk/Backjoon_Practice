2965e87c559173b10ff514130d517bfebd8fbee6
jaewoopk <jaewoopk000@naver.com>
2022-08-08 14:30:26 +0900

    Day 19 Solve 4 questions about combination

---
+++ B_11050_comb.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class B_11050_comb {
+    public static void main(String[] args) {
+        Integer n, k;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+        System.out.println(pactorial(n) / (pactorial(k) * pactorial(n - k)));
+
+    }
+    public static Integer pactorial(Integer num) {
+        if (num == 0)
+            return (1);
+        if (num == 1)
+            return (num);
+        return (num * pactorial(num - 1));
+    }
+}
