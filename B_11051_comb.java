b1d4f5062f5e96e2dec788e6e2153453590f03e1
jaewoopk <jaewoopk000@naver.com>
2022-08-09 15:05:53 +0900

    Day 20 Solve 1 question about combination with pascal triangle

--- B_11051_comb.java
+++ B_11051_comb.java
@@ -2,33 +2,20 @@ import java.util.*;
 
 public class B_11051_comb {
     public static void main(String[] args) {
-        Integer n, k, tmp1, tmp2, res;
+        Integer n, k;
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         k = sc.nextInt();
         
-        if (n - k == 0 || k == 0)
-        {
-            System.out.println("1");
-            return ;
+        int[][] Triangle = new int[n + 1][n + 1];
+
+        for (int i = 0; i < Triangle.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == j || j == 0) Triangle[i][j] = 1;
+                else Triangle[i][j] = (Triangle[i - 1][j - 1] + Triangle[i - 1][j]) % 10007;
+            }
         }
-        tmp1 = 1;
-        tmp2 = 1;
-        res = 1;
-        for (int i = 0; i < k; i++) {
-            tmp1 *= (n - i);
-            tmp2 *= (k - i);
-            res = tmp1 / tmp2;
-            if (res >= 10007)
-                res = (tmp1 / tmp2) % 10007;
-            if (tmp1 >= 10007)
-                tmp1 %= 9993;
-            if (tmp2 >= 10007)
-                tmp2 %= 10007;
-            System.out.println(res +" " +tmp1 +" "+ tmp2);
-        }
-        System.out.println(((12*11*10*9*8) / 120) % 10007);
-        System.out.println(res);
+        System.out.println(Triangle[n][k]);
     }
 }
