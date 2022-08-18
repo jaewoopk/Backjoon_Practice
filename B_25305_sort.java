65974339395a821c7ce54f4ef60ad218f4db3705
jaewoopk <jaewoopk000@naver.com>
2022-08-18 18:05:33 +0900

    Day 24 Solve 2 question with sort problem (its easy)

---
+++ B_25305_sort.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class B_25305_sort {
+    public static void main(String[] args) {
+        int n,m;
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<Integer>();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+        
+        Collections.sort(list);
+        System.out.println(list.get(n - m));
+    }
+}
