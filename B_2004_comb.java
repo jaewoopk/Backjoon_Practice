8520f3d84b35e3fe64fe13fc3e428d5549fb774b
jaewoopk <jaewoopk000@naver.com>
2022-08-18 16:46:43 +0900

    Day 24 Solve 1 question about combination with pactorial algorithm

--- B_2004_comb.java
+++ B_2004_comb.java
@@ -1,63 +1,40 @@
 import java.util.*;
+import java.math.*;
 
 public class B_2004_comb {
     public static void main(String[] args) {
         int n, m;
-        int[] arr = {0, 0, 0, 0};
+
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         m = sc.nextInt();
-        System.out.println(dismantling(n, m, arr, n));
+        System.out.println(dismantling(n, m));
     }
     
-    public static int dismantling(int num, int k, int[] arr, int t) {
-        int result1 = 0;
-        int result2 = 0;
+    public static int dismantling(int n, int m) {
+        int count5 = 0, count2 = 0;
 
-        if (k > t / 2)
-            k = t - k;
-        
-        for (int i = 0; i < k; i++) {
-            if ((num - i) % 5 == 0 || (num - i) % 2 == 0) {
-                sharing(num - i, arr);
-            }
-            if ((i + 1) % 5 == 0 || (i + 1) % 2 == 0) {
-                sharing2(i + 1, arr);
-            }
-        }
-        System.out.println("arr0 = " + arr[0] + " arr1 = "+ arr[1] + "\narr2 = " + arr[2] + " arr3 = "+ arr[3]);
-        result1 = (arr[0] > arr[1]) ? arr[1] : arr[0];
-        result2 = (arr[2] > arr[3]) ? arr[3] : arr[2];
-        return (result1 - result2);
+        count5 = sharing5(n) - sharing5(n - m) - sharing5(m);
+        count2 = sharing2(n) - sharing2(n - m) - sharing2(m);
+        return (Math.min(count5, count2));
     }
-    public static void sharing(int num, int[] arr) {
-        while(num > 0) {
-            if (num % 5 == 0) {
-                arr[0]++;
-                num /= 5;
-            }
-            else if (num % 2 == 0) {
-                arr[1]++;
-                num /= 2;
-            }
-            else
-                break ;
-        }
-    }
+    public static int sharing5(int num) {
+        int count = 0;
 
-    public static void sharing2(int num, int[] arr) {
-        while(num > 0) {
-            if (num % 5 == 0) {
-                arr[2]++;
-                num /= 5;
-            }
-            else if (num % 2 == 0) {
-                arr[3]++;
-                num /= 2;
-            }
-            else
-                break ;
+        while(num >= 5) {
+            count += num / 5;
+            num /= 5;    
         }
+        return count;
+    }
+    public static int sharing2(int num) {
+        int count = 0;
+
+        while(num >= 2) {
+            count += num / 2;
+            num /= 2;    
+        }
+        return count;
     }
 }
