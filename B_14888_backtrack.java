66ae02e2a6907beb3c9dac5f018b20d6c2f58e9a
jaewoopk <jaewoopk000@naver.com>
2022-08-23 17:34:40 +0900

    Day 28 Solve 1 question about backtracking but not my only opinion

--- B_14888_backtrack.java
+++ B_14888_backtrack.java
@@ -3,59 +3,38 @@ import java.util.*;
 public class B_14888_backtrack {
     static int max = -1000000001;
     static int min = 1000000001;
+    static int[] arr;
+    static int[] sign = new int[4];
+    static int n;
     public static void main(String[] args) {
-        int n;
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int arr[] = new int[n];
-        int sign[] = {0, 0, 0, 0};
+        arr = new int[n];
 
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
         for (int i = 0; i < 4; i++) sign[i] = sc.nextInt();
-        backtracking(arr, sign, 0, 0);
+        backtracking(arr[0], 1) ;
         System.out.println(max);
         System.out.println(min);
     }
 
-    public static void backtracking(int[] arr, int[] sign, int ans, int count) {
-        if (count >= arr.length - 1) {
-            if (ans > max) max = ans;
-            if (ans < min) min = ans;
+    public static void backtracking(int num, int idx) {
+        if (idx == n) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
             return ;
         }
-        else if (count == 0) ans = arr[0];
 
-        for (int i = count; i < arr.length - 1; i++) {
-            if (arr[count] != arr[i])
-                break ;
-            for (int j = 0; j < 4; j++) {
-                if (sign[j] == 0) continue;
-                if (j == 0) ans += arr[i + 1];
-                else if (j == 1) ans -= arr[i + 1]; 
-                else if (j == 2) ans *= arr[i + 1]; 
-                else if (j == 3) {
-                    if (ans < 0) {
-                        ans *= -1;
-                        ans /= arr[i + 1];
-                        ans *= -1;
-                    }
-                    else ans /= arr[i + 1];
+        for (int i = 0; i < 4; i++) {
+            if (sign[i] > 0) {
+                sign[i]--;
+                switch (i) {
+                    case 0 : backtracking(num + arr[idx], idx + 1); break;
+                    case 1 : backtracking(num - arr[idx], idx + 1); break;
+                    case 2 : backtracking(num * arr[idx], idx + 1); break;
+                    case 3 : backtracking(num / arr[idx], idx + 1); break;
                 }
-                sign[j]--;
-                backtracking(arr, sign, ans, ++count);
-                sign[j]++;
-                count--;
-                if (j == 0) ans -= arr[i + 1];
-                else if (j == 1) ans += arr[i + 1]; 
-                else if (j == 2) {
-                    if (ans < 0) {
-                        ans *= -1;
-                        ans /= arr[i + 1];
-                        ans *= -1;
-                    }
-                    else ans /= arr[i + 1];
-                }
-                else if (j == 3) ans *= arr[i + 1];
+                sign[i]++;
             }
         }
     }
