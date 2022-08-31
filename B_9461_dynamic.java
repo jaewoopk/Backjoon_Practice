4b07d004c0f7011ed69db877095bc2ecbb9c33e7
jaewoopk <jaewoopk000@naver.com>
2022-08-31 14:30:10 +0900

    Day 35 Solved 2 question about dynamic programming

---
+++ B_9461_dynamic.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class B_9461_dynamic {
+    static long[] arr = {1,1,1,2,2,3,4,5,7,9};
+    static long[] arr2 = new long[100];
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        for (int i = 0; i < 10; i++) {
+            arr2[i] = arr[i];
+        }
+        for (int i = 10; i < 100; i++) {
+            arr2[i] = arr2[i - 5] + arr2[i - 1];
+        }
+        for (int i = 0; i < n; i++) {
+            int p;
+            p = sc.nextInt();
+            if (p >= 1 && p <= 10) System.out.println(arr[p - 1]);
+            else System.out.println(arr2[p - 1]);
+        }
+    }    
+}
