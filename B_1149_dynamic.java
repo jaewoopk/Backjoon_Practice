6489056445071caaf60a0c4a6a4d1200af0c67e0
jaewoopk <jaewoopk000@naver.com>
2022-09-01 11:08:47 +0900

    Day 36 Solved no question about B_1149 Backtracking I think I can solve with dfs

--- B_1149_dynamic.java
+++ B_1149_dynamic.java
@@ -5,55 +5,35 @@ import java.util.*;
 
 public class B_1149_dynamic {
     static int[][] arr = new int[1000][3];
-    static int SUM = 0;
+    static int n;
+    static int SUM = 10000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int n = Integer.parseInt(st.nextToken());
-
+        n = Integer.parseInt(st.nextToken());
+        
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) arr[i][j] = Integer.parseInt(st.nextToken());
         }
-        int min = arr[0][0] > arr[0][1] ? arr[0][1] : arr[0][0];
-        TreeNode root = new TreeNode(Integer.min(min, arr[0][2]));
-        int idx;
-        if (min == arr[0][0]) idx = 0;
-        else if (min == arr[0][1]) idx = 1;
-        else idx = 2;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (idx == j) continue;
-                TreeNode newNode = new TreeNode(arr[i][j]);
-                if (root.left == null)
-                    root.left = newNode;
-                else
-                    root.right = newNode;
-            }
-        }
+        dfs(arr[0], 0, 0, 0);
         System.out.println(SUM);
-    }    
-}
-
-class TreeNode {
-    TreeNode left;
-    TreeNode right;
-    int value;
-
-    public TreeNode(int value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
     }
-    public TreeNode findLeftNode(TreeNode node) {
-        while (node.left != null) node = node.left;
-        return node;
-    }
-
-    public TreeNode findRightNode(TreeNode node) {
-        while (node.right != null) node = node.right;
-        return node;
+    public static void dfs(int[] del, int idx, int end, int sum) {
+        if (end == n - 1) {
+            SUM = Integer.min(SUM, sum);
+            System.out.println(SUM);
+            return ;
+        }
+        for (int i = 0; i < 3; i++) {
+            if (idx == i) continue;
+            System.out.println("i == " + i);
+            end++;
+            sum += del[i];
+            dfs(arr[end], i, end, sum);
+            sum -= del[i];
+            end--;
+        }
     }
 }
