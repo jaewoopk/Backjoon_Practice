2a66418484188093cd56aa99d73df01766a8d097
jaewoopk <jaewoopk000@naver.com>
2022-09-02 16:31:44 +0900

    Day 37 Solve 1 question B_2579 with dynamic programming

--- B_2579_dynamic.java
+++ B_2579_dynamic.java
@@ -4,23 +4,27 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class B_2579_dynamic {
-    static int[] arr;
-    static int n;
-    static int MAX = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        arr = new int[n];
-        for (int i = 0; i < n; i++) {
+    
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        int[] DP = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
             st = new StringTokenizer(br.readLine());
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = n -1; i > 1; i--) {
-            arr[n - 1] += Math.max(arr[i - 1], arr[i - 2]);
-            if (Math.max(arr[i - 1], arr[i - 2]) == arr[i - 2]) i--;
+
+        DP[1] = arr[1];
+
+        if (n >= 2) {
+            DP[2] = arr[1] + arr[2];
         }
-        System.out.println(arr[n - 1]);
+
+        for (int i = 3; i < n + 1; i++) {
+            DP[i] = Math.max(DP[i - 2], DP[i - 3] + arr[i - 1]) + arr[i];
+        }
+        System.out.println(DP[n]);
     }
 }
