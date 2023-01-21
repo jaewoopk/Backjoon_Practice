2d6ca874100b65e0bf32d3e6b5b1bf1654b5e00f
jaewoopk <jaewoopk000@naver.com>
2023-01-21 14:27:43 +0900

    Day 49 Solve B_2738 about array

---
+++ if & while & math/B_2738_array.java
@@ -0,0 +1,38 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_2738_array {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n, m;
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print((arr1[i][j] + arr2[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
