6920a9906746f702769b177c0b3f874c451587c9
jaewoopk <jaewoopk000@naver.com>
2023-02-01 22:26:22 +0900

    Day 54 Solve B_18258 about queue

---
+++ dataStructure/B_18258_queue.java
@@ -0,0 +1,32 @@
+package dataStructure;
+
+import java.util.Scanner;
+
+public class B_18258_queue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            if (str.equals("push")) {
+
+            }
+            else if (str.equals("pop")) {
+                
+            }
+            else if (str.equals("size")) {
+                
+            }
+            else if (str.equals("empty")) {
+                
+            }
+            else if (str.equals("front")) {
+                
+            }
+            else if (str.equals("back")) {
+                
+            }
+        }
+        sc.close();
+    }
+}
